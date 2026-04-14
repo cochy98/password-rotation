d2f1be421b24be928ad2532d38c9088967ddf88e
Cosmo Ferrigno <cosmoferrigno@gmail.com>
2026-04-14 16:06:02 +0200

    Fix password validator

--- src/Http/Requests/ChangePasswordRequest.php
+++ src/Http/Requests/ChangePasswordRequest.php
@@ -3,6 +3,7 @@
 namespace Cosmoferrigno\PasswordRotation\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 
 class ChangePasswordRequest extends FormRequest
@@ -16,7 +17,17 @@ class ChangePasswordRequest extends FormRequest
     {
         return [
             'current_password'      => ['required', 'string', 'current_password'],
-            'password'              => ['required', 'string', 'confirmed', Password::min(8)],
+            'password'              => [
+                'required',
+                'string',
+                'confirmed',
+                Password::min(8),
+                function (string $attribute, mixed $value, \Closure $fail): void {
+                    if (Hash::check($value, $this->user()->password)) {
+                        $fail('La nuova password deve essere diversa da quella attuale.');
+                    }
+                },
+            ],
             'password_confirmation' => ['required', 'string'],
         ];
     }
